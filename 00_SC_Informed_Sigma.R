25036e49521b74a0db3a5398caa564d5cb19593f
Burgar <Joanna.Burgar@gov.bc.ca>
2021-10-06 12:05:57 -0700

    Updating for adult marten home range sizes.

--- 00_SC_Informed_Sigma.R
+++ 00_SC_Informed_Sigma.R
@@ -25,11 +25,6 @@
 # home range size in ha
 # detection range in metres
 
-# for martens in sub-boreal spruce biogeoclimatic zone
-# Lofroth's 1993 MSc thesis, telemetry data suggested
-# adult male marten home range mean size = 5.52 km2, range 2.95-8.53 km2
-# adult female marten home range mean size = 4.55 km2m range 1.25-10.05 km2
-
 sigma_prior <- function (hrsize = hrszie, detect_min = detect_min, detect_max = detect_max){
   hrlength <- sqrt(hrsize/pi*10000)
 
@@ -43,11 +38,11 @@ sigma_prior <- function (hrsize = hrszie, detect_min = detect_min, detect_max = 
   sigma_lower <- sqrt(hr_detect_min_area*10000/pi)/sqrt(5.99)   # 187 m
   sigma_upper <- sqrt(hr_detect_max_area*10000/pi)/sqrt(5.99)  # 268 m
 
-  # Assuming a grid spacing of 1 unit = 100 m, aim to have a prior with most
+  # Assuming a grid spacing of 1 unit = 1000 m or 1 km, aim to have a prior with most
   # of the density between:
 
-  pd_lower <- sigma_lower/100
-  pd_upper <- sigma_upper/100
+  pd_lower <- sigma_lower/1000
+  pd_upper <- sigma_upper/1000
 
   return(list(pd_lower, pd_upper))
 }
@@ -55,21 +50,27 @@ sigma_prior <- function (hrsize = hrszie, detect_min = detect_min, detect_max = 
 
 ###---
 # now use the output to determine the range of dgamma by adjusting the last shape and scale
-# one example, for a home range of 40 ha with a call detection range between 100-300 m
-# hrsize <- 10
-# detect_min <- 100
-# detect_max <- 300
 
-# two example, for a home range of 10 ha no detection range
-# hrsize <- 10
+# for martens in sub-boreal spruce biogeoclimatic zone
+# Lofroth's 1993 MSc thesis, telemetry data suggested
+# adult male marten home range mean size = 5.52 km2, range 2.95-8.53 km2
+# adult female marten home range mean size = 4.55 km2m range 1.25-10.05 km2
+
+# to find out the range go with 125 ha to 1005 ha, with the bulk centred on 500 ha
+# hrsize <- 125 # lower
+# hrsize <- 1005 # upper
+# hrsize <- 500 # mid
 # detect_min <- 0
 # detect_max <- 0
 
-sigma_prior(hrsize = 10, detect_min = 0, detect_max = 0)
-# [1] 0.7289734
-# so in this example, want the density of the simga prior at 0.7289734 so a peak ~0.7
-# for a SC or SCR model, we want 2*sigma spacing of cameras or 0.7*2 = 1.46
-# assuming a grid spacing of 100 m we want cameras spaced 1.46 units or ~ 146 m
+sigma_prior(hrsize = 500, detect_min = 0, detect_max = 0)
+# [1] 0.258
+# so in this example, want the density of the simga prior to range between 0.258 and 0.731
+# with a peak at 0.515
+ 
+# for a SC or SCR model, we want 2*sigma spacing of cameras ~ 0.515*2 = 1.03 km (~0.5 - 1.5 km)
+# no smaller than 0.258*2 = 0.516 km and no greater than 0.731*2 = 1.462 km
+# assuming a grid spacing of 1000 m we want traps spaced ~ 1 km apart
 
 qgamma(c(0.001,0.5,0.999),60,27) #  1.439910 2.209889 3.215138 - tight home ranges centred on 40
 curve(dgamma(x,60,27), col='black',xlim=c(0,5), ylim=c(0,2))
