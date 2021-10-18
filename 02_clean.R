f3beb0efcba83cb6537a17d13676dd946a3ad5b9
Burgar <Joanna.Burgar@gov.bc.ca>
2021-10-18 14:39:15 -0700

    Cleaned up to output clean output objects for both the retrospective and current data.

--- 02_clean.R
+++ 02_clean.R
@@ -19,7 +19,7 @@
 .libPaths("C:/Program Files/R/R-4.0.5/library") # to ensure reading/writing libraries from C drive
 
 # Load Packages
-list.of.packages <- c("tidyverse", "lubridate","chron","sf","sp","raster","rgdal", "Cairo","OpenStreetMap", "ggmap")
+list.of.packages <- c("tidyverse", "lubridate","chron","sf","sp","raster","rgdal", "concaveman","Cairo","OpenStreetMap", "ggmap")
 
 # Check you have them and load them
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -139,186 +139,163 @@ save(marten.data, file = paste0("./out/MartenData_",survey.open.year,".Rda"))
 # summary(marten.data$observations) # checked all Rda objects = appear correct
 
 ############################--- CURRENT DATA ---###########################
-glimpse(hstrap)
-summary(hstrap)
-
-# hsdat <- hsdat %>% filter(Comments=="Sample genotyped")
-# try SCR with thinning with random thinning for unidentified encounters
-# https://onlinelibrary.wiley.com/doi/10.1002/ece3.7091
-# includes the unidentified but genotyped samples
-
-recaps <- hsdat %>% count(`Animal ID`)
-recaps %>% summarise(mean(n), min(n), max(n), se=sd(n)/sqrt(nrow(recaps)))
-recaps %>% filter(n==1) # 30 animals only caught once
-recaps %>% filter(n==2) # 4 animals caught twice
-recaps %>% filter(n==3) # 3 animals caught three times
 
 ###--- wrangle trap data
 traps.df <- as.data.frame(hstrap)
 names(traps.df)
 traps.df <- traps.df %>% dplyr::select(-`UTM Zone Sample Station`) %>% rename("Station"=1, "Easting"=2, "Northing"=3, "Grid_Cell"=4)
 traps.df %>% count(Grid_Cell) # 106 grid cells
-traps <- data.frame(Site = traps.df$Station, x = traps.df$Easting, y = traps.df$Northing)
-traps$TrapNumber <- 1:nrow(traps.df) # Index the traps by order 
+traps <- data.frame(Grid_Cell = traps.df$Grid_Cell, x = traps.df$Easting, y = traps.df$Northing)
+traps <- traps[!duplicated(traps$Grid_Cell),]
+traps <- traps %>% arrange(Grid_Cell)
 
-# 4 sampling occasions 
-hstrap %>% count(`Grid Cell`)
+traps$Grid_Num <- 1:nrow(traps) # Index the traps by order 
 
-# # create a trap operability matrix
-# tmp1 <- as.data.frame(lttrap[[i]])
-# tmp1$TrapNumber <- traps$TrapNumber[match(tmp1$Trap_ID,traps$Site)]
-# tmp1 <- tmp1[order(tmp1$TrapNumber),] # to ensure order by TrapNumber
-# 
-# tmp2 <- t(tmp1[4:ncol(tmp1)])
-# colnames(tmp2) <- tmp1$TrapNumber
-# 
-# trap.oper <- matrix(NA, nrow=nrow(daylookup),ncol=0)
-# rownames(trap.oper) <- as.character(daylookup$Date)
-# sum(tmp1[,4:ncol(lttrap[[i]])]) # 3362 occasions traps were open
-# 
-# trap.oper <- merge(trap.oper, tmp2, by="row.names", all.x=TRUE)
-# trap.oper[is.na(trap.oper)] <- 0
-# rownames(trap.oper) <- trap.oper[,1]
-# trap.oper <- trap.oper[2:ncol(trap.oper)]
+# Add covariate to determine if the trap was in a fisher or marten cell (i.e., cell size varies)
+traps.df$tmp <- str_count(traps.df$Grid_Cell, "-")
+traps.df$Grid_Focus <- ifelse(traps.df$tmp==3, "marten","fisher")
+traps.df$tmp <- NULL
+traps.df %>% filter(Grid_Focus=="fisher") %>% count(Grid_Cell)# 66 fisher focused cells
+traps.df %>% filter(Grid_Focus=="marten") %>% count(Grid_Cell)# 40 marten focused cells
 
-# dim(trap.oper)
-# colSums(trap.oper) 
-# rowSums(trap.oper)
-# sum(trap.oper)
+traps.sf <- st_as_sf(traps.df, coords=c("Easting","Northing"), crs=26910)
+ggplot()+
+  geom_sf(data=traps.sf, aes(fill=Grid_Focus, col=Grid_Focus))
 
-# trap.oper <- t(trap.oper) # trap operability matrix with rows as traps (row name = TrapNumber) and columns as Date (occasion)
+
+# tmp <- gsub(".*\\-", "", traps.sf$Grid_Cell)
+# traps.sf$Grid <- as.factor(substr(traps.sf$Grid_Cell, 1, nchar(traps.sf$Grid_Cell)-nchar(tmp)-1))
+# traps.sf %>% count(Grid) %>% st_drop_geometry() # only 
 
 
 ###--- wrangle detection data
+hsdat <- hsdat %>% filter(Comments=="Sample genotyped")
+# comment out above code if trying with random thinning
+# try SCR with thinning with random thinning for unidentified encounters
+# https://onlinelibrary.wiley.com/doi/10.1002/ece3.7091
+# includes the unidentified but genotyped samples
+
+recaps <- hsdat %>% count(`Animal ID`)
+nrow(hsdat) # 47 detections
+recaps %>% summarise(mean(n), min(n), max(n), se=sd(n)/sqrt(nrow(recaps)))
+nrow(recaps) # 37 individuals detected
+# `mean(n)` `min(n)` `max(n)`     se
+#   1      1.27        1        3 0.0999
+recaps %>% filter(n==1) # 30 animals only caught once
+recaps %>% filter(n==2) # 4 animals caught twice
+recaps %>% filter(n==3) # 3 animals caught three times
+
 hsdat %>% count(`Sampling Session`)
-hsdat$Occ <- as.factor(str_sub(hsdat$`Sampling Session`,-1))
+hsdat.mart <- hsdat
+hsdat.mart$Occ <- as.factor(str_sub(hsdat.mart$`Sampling Session`,-1))
+hsdat.mart <- hsdat.mart %>% dplyr::select(-`Study Area Name`, -Species, -`Sampling Session`) %>% rename("Station"=1, "Animal_ID"=3)
+hsdat.mart$Date <- ymd(hsdat.mart$Date)
+hsdat.mart$Grid_Cell <- traps.df$Grid_Cell[match(hsdat.mart$Station, traps.df$Station)]
+as.data.frame(hsdat.mart %>% arrange(Grid_Cell, Station))
+as.data.frame(hsdat.mart %>% group_by(Grid_Cell, Station) %>% count(Occ))
+count(hsdat.mart, Station) # 44 stations with detections
+count(hsdat.mart, Grid_Cell) # 42 grid cells with detections
 
-hsdat$Grid_Cell <- traps.df$Grid_Cell[match(hsdat$Station, traps.df$Station)]
-hsdat %>% count(Grid_Cell)
-as.data.frame(hsdat %>% group_by(Grid_Cell, Station) %>% count(Occ))
-hsdat.mart <- hsdat %>% filter(Animal_ID!="M_xxxx") %>% dplyr::select(Station,Occ)
-# Match trap numbering and dat.mart
-hsdat.mart$TrapNumber <- traps$TrapNumber[match(hsdat.mart$Station, traps$Site)]
-hsdat.mart$Count <- 1
-observations <- hsdat.mart %>% dplyr::select(-Station) %>% 
-  pivot_wider(names_from=Occ, values_from=Count, values_fill = 0)
-observations <- as.data.frame(observations[order(observations$TrapNumber),])
-rownames(observations) <- as.character(observations$TrapNumber)
-observations <- observations[c("TrapNumber","1","2","3","4")]
-sum(observations[2:5]) # 47 detections
+# Create numeric values for animal ID and grid cells
+# animal ID
+edf <- hsdat.mart %>% dplyr::select(Animal_ID, Occ, Grid_Cell, Sex)
+edf <- edf %>% arrange(Animal_ID, Occ, Grid_Cell, Sex)  
 
-n <- matrix(0, nrow=nrow(traps),ncol=0)
-rownames(n) <- as.character(traps$TrapNumber)
-n <- merge(n, observations, by="row.names", all.x=TRUE)
-n <- n %>% dplyr::select(-Row.names, -TrapNumber)
-n[is.na(n)] <- 0
-sum(n) # 47 detections - matches with observations
+animal <- edf[!duplicated(edf$Animal_ID),]
+animal <- animal %>% arrange(Animal_ID)
+animal$Animal_Num <- row.names(animal)
+
+sex <- animal$Sex
+sex <- ifelse(sex=="M",0,1) # 0 is male, 1 is female
+# Now put it on the animal:
+
+edf$Animal_Num <- as.numeric(animal$Animal_Num[match(edf$Animal_ID, animal$Animal_ID)])
+edf$Sex <- as.numeric(ifelse(edf$Sex=="M",0,1))
+edf$Grid_Num <- as.numeric(traps$Grid_Num[match(edf$Grid_Cell, traps$Grid_Cell)])
+
+
+# create detection histories for each marten, by Grid cell rather than station
+# keeping code in case need to copy in other scripts, but going with edf code above for this project
+# first create larger detection history table
+# hsdat.mart$Count <- 1
+# observations <- as.data.frame(hsdat.mart  %>% dplyr::select(Animal_ID, Occ, Count, Grid_Cell) %>% 
+#   pivot_wider(names_from=Occ, values_from=Count, values_fill = 0))
+# observations <- as.data.frame(observations[order(observations$Grid_Cell),])
+# observations <- observations[c("Grid_Cell","Animal_ID","1","2","3","4")]
+# sum(observations[c("1","2","3","4")]) # 47 detections
+# 
+# unique.marten <- unique(observations$Animal_ID)
+# 
+# marten.dh <- array(0, c(37,106,4))
+# marten.dh
+# dim(marten.dh)
+# i=1
+# for(i in 1:length(unique.marten)){
+#   indi.obs <- matrix(NA, nrow=length(unique(traps.df$Grid_Cell)),ncol=0)
+#   rownames(indi.obs) <- as.character(unique(traps.df$Grid_Cell))
+#   tmp <- observations %>% filter(Animal_ID==unique.marten[i]) %>% dplyr::select(-Animal_ID)
+#   rownames(tmp) <- as.character(tmp$Grid_Cell)
+#   tmp$Grid_Cell <- NULL
+#   indi.obs <- merge(indi.obs, tmp, by="row.names", all.x=TRUE)
+#   indi.obs[is.na(indi.obs)] <- 0
+#   indi.obs <- indi.obs[2:ncol(indi.obs)]
+#   marten.dh[i,,] <- indi.obs
+# }
+
 
 # Scale to km
 coord.scale <- 1000
 traps.scale <- traps
 traps.scale <- traps[,c("x", "y")]/coord.scale
 
-# Create a buffer around the traps by 15km.
-pts <- SpatialPoints(traps.scale[,c("x", "y")])
-b.r <- buffer(pts, width = 15, dissolve = TRUE)
-plot(b.r)
-points(pts, col = "red", pch = 4)
-bb <- bbox(b.r)
+buffer <- 5 #5 km unit buffer
 
-# plot captures and recaptures
-hsdat$Easting <- traps.df$Easting[match(hsdat$Station, traps.df$Station)]
-hsdat$Northing <- traps.df$Northing[match(hsdat$Station, traps.df$Station)]
+traps.sc <- as.data.frame(cbind(traps.scale$x-min(traps.scale$x-buffer), traps.scale$y-min(traps.scale$y-buffer)))
 
-cap1.coords <- hsdat %>% filter(Animal_ID %in% recaps[recaps$n==1,]$`Animal ID`) %>% dplyr::select("Easting", "Northing")
-cap1.coords.scaled <- cap1.coords/coord.scale
-cap1 <- SpatialPoints(cap1.coords.scaled[,c("Easting", "Northing")])
+xlim = range(traps.sc[,1])+c(-buffer,buffer)
+ylim = range(traps.sc[,2])+c(-buffer,buffer)
+area <- diff(xlim)*diff(ylim)/100	# Density reported per 100 sq km
 
-cap2.coords <- hsdat %>% filter(Animal_ID %in% recaps[recaps$n==2,]$`Animal ID`) %>% dplyr::select("Easting", "Northing")
-cap2.coords.scaled <- cap2.coords/coord.scale
-cap2 <- SpatialPoints(cap2.coords.scaled[,c("Easting", "Northing")])
+# other data to save
+marten.hsdata <- list(J = nrow(traps),
+                      area = area,
+                      xlim = xlim,
+                      ylim = ylim,
+                      traps = traps.sc,
+                      edf = edf)
 
-cap3.coords <- hsdat %>% filter(Animal_ID %in% recaps[recaps$n==3,]$`Animal ID`) %>% dplyr::select("Easting", "Northing")
-cap3.coords.scaled <- cap3.coords/coord.scale
-cap3 <- SpatialPoints(cap3.coords.scaled[,c("Easting", "Northing")])
-
-b.r <- buffer(pts, width = 15, dissolve = TRUE)
-plot(b.r)
-points(pts, col="black", pch=19, cex=0.5)
-points(cap1.coords.scaled, col = "red", pch = 4,  cex=1.5)
-points(cap2.coords.scaled, col = "blue", pch = 4, cex=1.5)
-points(cap3.coords.scaled, col = "green", pch = 4, cex=1.5)
-
-# models run better when centered on 0 (MCMC issue)
-traps.sc <- as.data.frame(cbind(traps.scale$x-bb[1,1], traps.scale$y-bb[2,1]))
-colnames(traps.sc) <- c("x","y")
-
-marten.hsdata <- list(xlim = bb[1,]-bb[1,1], ylim = bb[2,]-bb[2,1], traps = traps.scale, observations = n)
 save(marten.hsdata, file = paste0("./out/MartenData_2020.Rda"))
 
+###--- for visualization of recaps
 
-#### simulation to see what results should look like
-set.seed(10)
-xlim = bb[1,]-bb[1,1]; ylim = bb[2,]-bb[2,1]
-A <- (xlim[1] - xlim[2]) * (ylim[1] - ylim[2])
-A # area in km2 16099.01 
+# # Create a buffer around the traps (15 km)
+# For visulaization of recaps
+# pts <- SpatialPoints(traps.scale[,c("x", "y")])
+# b.r <- buffer(pts, width = 15, dissolve = TRUE)
+# plot(b.r)
+# points(pts, col = "red", pch = 4)
+# bb <- bbox(b.r)
 
-mu <- 0.01 # density per km2
-N <- rpois(1, mu*A) # generate population
-N # 161
+# plot captures and recaptures
+# hsdat.mart$Easting <- traps.df$Easting[match(hsdat.mart$Station, traps.df$Station)]
+# hsdat.mart$Northing <- traps.df$Northing[match(hsdat.mart$Station, traps.df$Station)]
+# 
+# cap1.coords <- hsdat.mart %>% filter(Animal_ID %in% recaps[recaps$n==1,]$`Animal ID`) %>% dplyr::select("Easting", "Northing")
+# cap1.coords.scaled <- cap1.coords/coord.scale
+# cap1 <- SpatialPoints(cap1.coords.scaled[,c("Easting", "Northing")])
+# 
+# cap2.coords <- hsdat.mart %>% filter(Animal_ID %in% recaps[recaps$n==2,]$`Animal ID`) %>% dplyr::select("Easting", "Northing")
+# cap2.coords.scaled <- cap2.coords/coord.scale
+# cap2 <- SpatialPoints(cap2.coords.scaled[,c("Easting", "Northing")])
+# 
+# cap3.coords <- hsdat.mart %>% filter(Animal_ID %in% recaps[recaps$n==3,]$`Animal ID`) %>% dplyr::select("Easting", "Northing")
+# cap3.coords.scaled <- cap3.coords/coord.scale
+# cap3 <- SpatialPoints(cap3.coords.scaled[,c("Easting", "Northing")])
+# 
+# b.r <- buffer(pts, width = 15, dissolve = TRUE)
+# plot(b.r)
+# points(pts, col="black", pch=19, cex=0.5)
+# points(cap1.coords.scaled, col = "red", pch = 4,  cex=1.5)
+# points(cap2.coords.scaled, col = "blue", pch = 4, cex=1.5)
+# points(cap3.coords.scaled, col = "green", pch = 4, cex=1.5)
 
-s <- data.frame(s.x = runif(N, xlim[1], xlim[2]),
-                s.y = runif(N, ylim[1], ylim[2]))
-
-sigma <- 0.5
-lambda0 <- 2
-J <- nrow(traps.sc) # nb of traps
-K <- ncol(n) # nb capture occasions
-
-
-yy <- array(NA, c(N, J, K))
-for(j in 1:J) {
-  dist <- sqrt((traps.sc$x[j] - s$s.x)^2 + (traps.sc$y[j] - s$s.y)^2)
-  lambda <- lambda0 * exp(-dist^2 / (2 * sigma^2))
-  for(k in 1:K) {
-    yy[,j,k] <- rpois(N, lambda)
-  }
-}
-n <- apply(yy, c(2,3), sum)
-tot <- apply(n, 1, sum)
-# sum(tot)
-dat <- data.frame(traps.sc, tot = tot)
-
-###--- SIMULATION 2 = using nimbleSCR
-# https://cran.r-project.org/web/packages/nimbleSCR/vignettes/Simulate_and_fit_SCR_models_with_dbinomLocal_normal.html
-
-  
-
-aoi=st_as_sf(b.r, crs=26910)
-class(aoi)
-aoi_grid <- sa_grid <- st_make_grid(aoi, cellsize=2, square=TRUE) #  grid for entire AOI (rectangle)
-sa_points <- st_point_on_surface(sa_grid)  # if using portion of aoi
-sa_points <- st_intersection(sa_points, aoi)
-
-head(st_coordinates(sa_points))
-
-coordsHabitatGridCenter <- as.data.frame(st_coordinates(sa_points))
-coordsHabitatGridCenter$X <- coordsHabitatGridCenter$X - min(coordsHabitatGridCenter$X)
-coordsHabitatGridCenter$Y <- coordsHabitatGridCenter$Y - min(coordsHabitatGridCenter$Y)
-colnames(coordsHabitatGridCenter) <- c("x","y")
-
-# ADD IN TRAP GRID
-trapCoords <- traps.sc
-colnames(trapCoords) <- c("x","y")
-
-# PLOT CHECK
-plot(coordsHabitatGridCenter[,"y"] ~ coordsHabitatGridCenter[,"x"], pch = 1, cex = 1.5) #pch=16) 
-points(trapCoords[,"y"] ~ trapCoords[,"x"], col="red", pch=16 ) 
-par(xpd=TRUE)
-legend(x = 7, y = 13,
-       legend=c("Habitat grid centers", "Traps"),
-       pt.cex = c(1.5,1),
-       horiz = T,
-       pch=c(1,16),
-       col=c("black", "red"),
-       bty = 'n')
