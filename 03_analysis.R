1dcde5ed10344cabe8beb7521bcf6736a6162cd4
Burgar <Joanna.Burgar@gov.bc.ca>
2021-10-29 12:00:15 -0700

    Including NIMBLE code and attempting SCR NIMBLE (current data), data wrangling for n-mixture NIMBLE (retrospective data)

--- 03_analysis.R
+++ 03_analysis.R
@@ -19,7 +19,7 @@
 .libPaths("C:/Program Files/R/R-4.0.5/library") # to ensure reading/writing libraries from C drive
 
 # Load Packages
-list.of.packages <- c("tidyverse","parallel", "nimbleEcology","nimble", "scrbook","nimbleSCR","basicMCMCplots","coda","Cairo")
+list.of.packages <- c("tidyverse","parallel","unmarked", "nimbleEcology","nimble", "scrbook","nimbleSCR","basicMCMCplots","coda","Cairo")
 
 # Check you have them and load them
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -30,7 +30,27 @@ rm(list.of.packages, new.packages) # for housekeeping
 ################################################################################
 # if issues running nimble, run the following line
 writeLines('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', con = "~/.Renviron")
+getwd()
 
+################################################################################
+e2dist <- function (x, y) {  # Function from scrbook package to calculate the distance between 
+  # locations in 2 matrices.
+  i <- sort(rep(1:nrow(y), nrow(x)))
+  dvec <- sqrt((x[, 1] - y[i, 1])^2 + (x[, 2] - y[i, 2])^2)
+  matrix(dvec, nrow = nrow(x), ncol = nrow(y), byrow = F)
+}
+################################################################################
+
+# Some of the NIMBLE examples generate html pages with comparisons
+# of different MCMCs.  These will be placed in the path contained in
+# the variable 'outputDirectory'.  If you do not provide 'outputDirectory',
+# the default location will be a subdirectory 'your_comparison_pages'
+# of your current working directory.  Your current working directory
+# can be obtained by getwd().
+if(!exists('outputDirectory')) {
+  outputDirectory <- file.path(getwd(), "your_comparison_pages")
+  dir.create(outputDirectory, showWarnings = FALSE)
+}
 
 ############################--- RETROSPECTIVE DATA ---##########################
 # Run N-mixutre model for unmarked live trap data
@@ -38,43 +58,150 @@ writeLines('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', con = "~/.Renviron")
 # Adapting from https://rdrr.io/cran/nimbleEcology/man/dNmixture.html
 # Also need to review Applied Hierarchical Modeling code (converted to NIMBLE and available on github)
 # https://github.com/nimble-training/AHMnimble/blob/master/Chapter_6/Section_6p11_setup.R
-
+# unmarked pcount: https://cran.r-project.org/web/packages/unmarked/unmarked.pdf
+# https://github.com/nimble-training/AHMnimble/tree/master/Chapter_6
 
 # load data if not running concurrently
 # load("out/MartenData_1996.Rda")
 glimpse(marten.data)
 
+# https://github.com/nimble-training/AHMnimble/blob/master/Chapter_6/Section_6p11_setup.R
 
-# Set up constants and initial values for defining the model
-len <- 5 # length of dataset
-dat <- c(1,2,0,1,5) # A vector of observations
-lambda <- 10 # mean abundance
+marten.data$observations
+marten.data$observations %>% count(Trap_ID)
+marten.data$trap.oper
+trap.oper <- marten.data$trap.oper
 
-# load
-prob <- c(0.2, 0.3, 0.2, 0.1, 0.4) # A vector of detection probabilities
+traps.open <- rowSums(trap.oper)
+traps.open[order(traps.open)]
+length(traps.open[traps.open >30])
 
-# Define code for a nimbleModel
-nc <- nimbleCode({
-  x[1:5] ~ dNmixture_v(lambda, prob = prob[1:5],
-                       Nmin = -1, Nmax = -1, len = 5)
+
+# add week and week occasion to the daylookup
+daylookup <- marten.data$daylookup
+glimpse(daylookup)
+daylookup$Week <- week(daylookup$Date)
+num.weeks <- round(nrow(daylookup)/7+1)
+week.occ <- rep(1:num.weeks, each = 7)
+daylookup$Occ_week <- week.occ[1:nrow(daylookup)]
+
+# create a covariate and dataset for weekly occasions
+# covariate is number of days trap open per week (0-7)
+colnames(trap.oper)
+
+# make sure to only use full weeks in occasion week (i.e., remove last week if not containing 7 days)
+weeks.to.use <- daylookup %>% count(Occ_week)
+weeks.to.use <- weeks.to.use[weeks.to.use$n==7,]$Occ_week
+
+# observation covariates need to be in dim(M,J) where M = number of sites, J = number of sampling occasions
+week.effort <- as.data.frame(array(NA, dim=c(nrow(trap.oper), length(weeks.to.use))))
+colnames(week.effort) <- weeks.to.use
+rownames(week.effort) <- rownames(trap.oper)
+
+count = 1 
+
+for(i in 1:length(week.effort)){
+  # i=8
+  tmp1 <- as.data.frame(t(trap.oper))
+  tmp1$Occ_week <- daylookup$Occ_week[match(rownames(tmp1), as.character(daylookup$Date))]
+  tmp1 <- tmp1 %>% filter(Occ_week %in% weeks.to.use)
+  tmp1 %>% count(Occ_week)
+  tmp2 <- tmp1 %>% filter(Occ_week==i) %>% colSums()
+
+  week.effort[,count] <- tmp2[1:nrow(trap.oper)]
   
-  lambda ~ dunif(0, 1000)
+  count <- count + 1
+}
+
+tot.effort <- rowSums(week.effort) # total effort per trap
+
+
+
+# for nmixture model, think we need to have traps open for the same length
+# try first with traps open for 40 days and grab data for first 40 days traps were open
+# write as a function to be able to easily grab different subsets of data
+num.days <- 30
+
+
+nmix.create.y.obs.cov <- function(num.days = num.days){
+  # i=1
+  trap.oper <- marten.data$trap.oper
+  traps.open <- rowSums(trap.oper)
+  tmp1 <- traps.open[traps.open > num.days]
+  tmp2 <- row.names(as.data.frame(tmp1))
+  tmp3 <- trap.oper[rownames(trap.oper)%in% tmp2,] # subset matrix to just traps open for the length (i.e., number of days) specified in num.traps
   
-  for (i in 1:5) {
-    prob[i] ~ dunif(0, 1)
+  observations <- marten.data$observations
+  tmp4 <- observations %>% filter(TrapNumber %in% tmp2) # filter observations to only traps open for the length (i.e., number of days) specified in num.traps
+  # tmp4 %>% count(TrapNumber)
+  
+  # need to grab first num.days length for each trap (e.g., if num.days is 30, grab first 30 days that trap is open)
+  # then find the observation data that corresponds to those dates
+  # add in the 1 when a marten was in a trap and a 0 if trap open but not in trap
+  # create a matrix appending rows as the loop cycles...not sure about this bit
+  
+  
+  y <- as.data.frame(array(NA, dim=c(length(tmp1), num.days))) # to create the y object
+  obs.cov <- as.data.frame(array(NA, dim=c(num.days,length(tmp1)))) # to create the date obs covariate
+  count <- 1
+  
+  
+  for(i in 1:length(tmp2)){
+    # i=8
+    tmp5 <- as.data.frame(t(tmp3[i,]))
+    tmp6 <- tmp5[order(tmp5>0, decreasing = TRUE)]
+    tmp7 <- tmp6[,1:num.days]
+    tmp8 <- as.data.frame(t(tmp7))
+    colnames(tmp8)[1] <- tmp2[i]
+    
+    sub.obs <- observations %>% filter(TrapNumber==tmp2[i]) 
+    sub.obs$Date_obs <- as.character(sub.obs$Date_obs)
+    tmp8$obs <- sub.obs$TrapNumber[match(rownames(tmp8), sub.obs$Date_obs)]
+    tmp8$obs[is.na(tmp8$obs)] <- 0
+    y[count,] <- t(tmp8$obs)
+    
+    tmp8$Day <- daylookup$Day[match(rownames(tmp8), daylookup$Date)]
+    obs.cov[count,] <- (tmp8$Day)
+    
+    count <- count + 1
   }
-})
+  
+  y <- ifelse(y>0,1,0)
+  colnames(y) <- seq(1:num.days)
+  rownames(y) <- tmp2
+  
+  colnames(obs.cov) <- seq(1:num.days)
+  rownames(obs.cov) <- tmp2
+  
+  return(list(y, obs.cov))
+}
 
-# Build the model
-nmix <- nimbleModel(nc,
-                    data = list(x = dat),
-                    inits = list(lambda = lambda,
-                                 prob = prob))
-# Calculate log probability of data from the model
-nmix$calculate()
+# y = R (number of sites/traps) x J (number of sampling periods) with y as repeated counts
+y40 <- nmix.create.y.obs.cov(num.days=40)
+y50 <- nmix.create.y.obs.cov(num.days=50)
+y10 <- nmix.create.y.obs.cov(num.days=10)
+str(y10)
+sum(y10[[1]])
+
+y = as.matrix(y10[[1]]) # with rows as traps (rowname = Trap_Num) and columns as sampling periods (1:30 when traps were open)
+dim(y)
+obs.cov = array(y10[[2]])
+dim(obs.cov)
+umf <- unmarkedFramePCount(y=y, obsCovs = obs.cov)
+summary(umf)
+fm_P <- pcount(~1 ~1, umf, K=100, mixture = "ZIP", engine=c("C")) # not sure if it's working, just keeps running
+?pcount
+
+
+###---
+# NIMBLE conversion of AHM book
+# https://github.com/nimble-training/AHMnimble/blob/master/Chapter_6/Section_6p3_example_nimble.R
+
 
 ############################--- CURRENT DATA ---###########################
 ############################--- NIMBLE CODE ---###########################
+
+?dunif
 
 ###--- SCR nimble code from Paul van Dam-Bates
 SCR_bern_sex <- nimbleCode({
@@ -82,7 +209,7 @@ SCR_bern_sex <- nimbleCode({
   # sigma ~ dgamma(20,20) # uninformative prior
   psi ~ dbeta(1,1)
   psex ~ dbeta(1,1)
-  lambda ~ dunif(0,10)
+  lambda ~ dunif(0,100)
   
   for(i in 1:M){
     sex[i] ~ dbern(psex)
@@ -103,10 +230,9 @@ SCR_bern_sex <- nimbleCode({
 })
 
 SCR_bern <- nimbleCode({
-  sigma ~ dunif(0,1000) # uninformative prior
-  # sigma ~ dgamma(20,20) # uninformative prior
+  sigma ~ dunif(0,100) # uninformative prior
   psi ~ dbeta(1,1)
-  lambda ~ dunif(0,10)
+  lambda ~ dunif(0,100)
   
   for(i in 1:M){
     z[i] ~ dbern(psi)
@@ -123,6 +249,7 @@ SCR_bern <- nimbleCode({
 ############################--- COMPILING DATA ---###########################
 ###--- actual data
 load("out/MartenData_2020.Rda")
+load("out/MartenGridData_2020.Rda")
 
 J <- marten.hsdata$J
 area <- marten.hsdata$area
@@ -133,10 +260,22 @@ edf <- marten.hsdata$edf
 trials <- rep(4,nrow(traps))
 sex <- marten.hsdata$sex
 
-M <- 1000
-y <- array(0, dim = c(M, 106, 4))
+J <- martenGrid.hsdata$J
+area <- martenGrid.hsdata$area
+xlim <- martenGrid.hsdata$xlim
+ylim <- martenGrid.hsdata$ylim
+traps <- martenGrid.hsdata$traps
+edf <- martenGrid.hsdata$edf
+trials <- rep(4,nrow(traps))
+sex <- martenGrid.hsdata$sex
+
+
+M <- 500
+y <- array(0, dim = c(M, J, 4))
 # Add the captures as 1s with the good old cbind trick.
+dim(y)
 glimpse(edf)
+edf$Occ <- as.numeric(edf$Occ)
 y[cbind(edf$Animal_Num,edf$Grid_Num, edf$Occ)] <- 1
 sum(y) == nrow(edf) 	# It worked right?
 
@@ -163,35 +302,40 @@ data <- list(y = y_all,
 
 ###--- Simulated data
 # simulation to see what results should look like
+# from Rich Weir
+# if we figure a male home range might be 5 km² and female about 2 km², that would be 0.75 martens/km² in 'occupied habitat'
+# but that only ~15% of the landscape would have martens... I would guess around 10-15 martens/100 km²
+# that's probably the lower end... maybe up to 25 martens/100 km²?
+
 set.seed(10)
 
 area # Density reported per 100 sq km
 
-mu <- 5 # density per 100 km2
-N <- rpois(1, mu*area) # generate population
-N # 554
+mu <- 15 # density per 100 km2
+N <- rpois(1, mu*area) # generate population (area is already in 100 sq km units)
+N # 1646
 
 s <- data.frame(s.x = runif(N, xlim[1], xlim[2]),
                 s.y = runif(N, ylim[1], ylim[2]))
 
-sigma <- 1
-lambda0 <- 1
+sigma <- 3
+lambda0 <- 0.1
 J <- nrow(traps) # nb of traps
 K <- length(unique(edf$Occ)) # nb capture occasions
 
-M <- 1000
+M <- 2000
 
 # Now let's speed it up by summing over all 4 nights for a binomial dist.
 yy <- array(NA, c(N, J, K))
 for(j in 1:J) {
-  dist <- sqrt((traps.sc$x[j] - s$s.x)^2 + (traps.sc$y[j] - s$s.y)^2)
+  dist <- sqrt((traps$x[j] - s$s.x)^2 + (traps$y[j] - s$s.y)^2)
   lambda <- lambda0 * exp(-dist^2 / (2 * sigma^2))
   for(k in 1:K) {
     yy[,j,k] <- rpois(N, lambda)
   }
 }
 n_all <- apply(yy, 1:2, sum)
-sum(n_all) # 75 animals in the detection matrix
+sum(n_all) # 353 animals in the detection matrix
 dim(n_all)
 
 M_rest <- array(0, c(M-nrow(n_all), ncol(n_all)))
@@ -206,14 +350,62 @@ sim.data.sex <- list(y = sim_all,
 sim.data <- list(y = sim_all, 
                      z =  c(rep(1, max(nrow(n_all))), rep(NA, M-max(nrow(n_all)))))
 
+
+###--- Inits
+
+# Initial values in NIMBLE must be carefully tailored to ensure that all nodes,
+# particularly for latent y.true, begin at reasonable initial values. Any error
+# here could invalidate the results
+ys<-apply(yaug,c(1,2),sum)
+s.start <- cbind(runif(M, xlim[1], xlim[2]), runif(M, ylim[1], ylim[2]))
+d <- e2dist(s.start[1:M,], traps)
+lam0s<- runif(1,0.1,0.3)
+sigs <- runif(1,1,5)
+K <- 4
+
+lam <- lam0s * exp( -(d^2)/(2 * sigs^2))
+
+nnind <- nrow(edf) # Number of individuals detected
+nnid <- M_rest
+yi <- array(0, c(M, J, K)) # resighting array
+for (j in 1:J) {
+  for (k in 1:K) {
+    if (nnid[j, k] > 0) {
+      probs <- lam[ ,j]
+      probs <- probs / sum(probs)
+      latent.id <- sample(1:M, nnid[j,k], prob = probs, replace = FALSE)
+      yi[latent.id , j, k] <- 1
+    }
+  } # end of k
+}   # end of j
+
+yis<-apply(yi,c(1,2),sum) + apply(yaug,c(1,2),sum)
+zst<-apply(yis, 1, sum); zst[zst>0]<-1
+id.prob.s<-sum(yaug)/(sum(yaug)+sum(nnid))
+
+inits <-   list(z=zst,             # z inits
+                s=s.start,         # s inits
+                lam0=lam0s,        # baseline detection rate
+                sig=sigs,          # movement parameter
+                id.prob=id.prob.s, # detection rate
+                y.full=yis)        # latent true histories
+str(inits)
+## List of 6
+##  $ z      : num [1:80] 1 1 1 1 1 1 1 1 0 0 ...
+##  $ s      : num [1:80, 1:2] 10.31 13.03 11.85 11.67 5.43 ...
+##  $ lam0   : num 0.281
+##  $ sig    : num 0.462
+##  $ id.prob: num 0.0758
+##  $ y.full : num [1:80, 1:144] 0 0 0 0 0 0 0 0 0 0 ...
+
 ############################--- RUN MODEL ---###########################
 # run for both simulated data and actual data
-Rmodel <- nimbleModel(SCR_bern_sex, constants, sim.data.sex)
-Rmodel <- nimbleModel(SCR_bern, constants, sim.data)
+# Rmodel <- nimbleModel(SCR_bern_sex, constants, sim.data.sex)
+Rmodel <- nimbleModel(SCR_bern_sex, constants, data.sex)
 
 conf <- configureMCMC(Rmodel)
-# conf$setMonitors(c('sigma', 'lambda', 'psi', 'N', 'D', 'psex'))
-conf$setMonitors(c('sigma', 'lambda', 'psi', 'N', 'D'))
+conf$setMonitors(c('sigma', 'lambda', 'psi', 'N', 'D', 'psex'))
+# conf$setMonitors(c('sigma', 'lambda', 'psi', 'N', 'D'))
 
 # Use a block update on locations. Saves time.
 conf$removeSamplers('X')
@@ -224,7 +416,7 @@ Rmcmc <- buildMCMC(conf)
 Cmodel <- compileNimble(Rmodel)
 Cmcmc <- compileNimble(Rmcmc, project = Rmodel)
 
-# Run compiled model. Takes ~11 minutes on my machine (M=300, niter=10000) or ~40 minutes (M=500, niter=20000).
+# Run compiled model. Takes ~11 minutes on my machine (M=300, niter=10000) or ~40 minutes (M=500, niter=20000) or ~60 min (M=2000, niter=10000).
 print(Sys.time())
 samplesList <- runMCMC(Cmcmc,
                        niter = 10000,
