8ff6323d9a5eb3feaab0dd7b5768ad28ad094ae5
Burgar <Joanna.Burgar@gov.bc.ca>
2022-03-16 23:12:09 -0700

    Updating for dynamic occupancy models and smaller grid cell size.

--- 03_analysis_occupancy.R
+++ 03_analysis_occupancy.R
@@ -33,53 +33,88 @@ rm(list.of.packages, new.packages) # for housekeeping
 
 ############################--- LOAD DATA ---##########################
 
-load("out/retro.data.out.RData")
-# load("out/rec.data.out.RData")
-cov.df <- read.csv("data/retro.covdata.1997.csv",row.names=1)
-# cov.df <- read.csv("data/rec.covdata.2020.csv",row.names=1)
+load("out/retro.data.out9697.RData")
+load("out/rec.data.out.RData")
+
+cov.df <- read.csv("data/covdata.csv",row.names=1)
+head(cov.df)
+# cov.data.files <- c("data/aoi.covdata.1996.csv","data/retro.covdata.1996.csv",
+#                     "data/aoi.covdata.1997.csv","data/retro.covdata.1997.csv",
+#                     "data/aoi.covdata.2020.csv","data/rec.covdata.2020.csv")
+# 
+# ALLcov.df <-do.call(rbind,lapply(cov.data.files, read.csv))
+# ALLcov.df$X <- ALLcov.df$grid_id <- NULL
+# cov.df.nrows <- sapply( cov.data.files, function(f) nrow(read.csv(f)) )
+# 
+# ALLcov.df$Year <- c(rep(1996,cov.df.nrows[1]+cov.df.nrows[2]),
+#                     rep(1997,cov.df.nrows[3]+cov.df.nrows[4]), 
+#                     rep(2019,cov.df.nrows[5]+cov.df.nrows[6]))
+# ALLcov.df$Area <- c(rep("Area",cov.df.nrows[1]), rep("Traps",cov.df.nrows[2]),
+#                     rep("Area",cov.df.nrows[3]), rep("Traps",cov.df.nrows[4]),
+#                     rep("Area",cov.df.nrows[5]), rep("Traps",cov.df.nrows[6]))
+# ALLcov.df %>% group_by(Year) %>% count(Area)
+# ALLcov.df$Area_Year <- paste(ALLcov.df$Area, ALLcov.df$Year)
 
 
-# ALLcov.df <- as.data.frame(rbind(read.csv("data/retro.covdata.1996.csv",row.names=1),
-#                                  read.csv("data/retro.covdata.1997.csv",row.names=1),
-#                                  read.csv("data/rec.covdata.2020.csv",row.names=1)))
-# ALLcov.df$Year <- c(rep(1996,38),rep(1997,38), rep(2020,86))
+cov.df <- cov.df %>% filter(grid_touse==1) %>% select(-use, -grid_touse,-RLW_type)
+head(cov.df)
+cov.df <- cov.df %>% rename("TRP_DNSTY_96"=TRP_DNSTY_9195, "TRP_DNSTY_97"=TRP_DNSTY_9296, "TRP_DNSTY_19"=TRP_DNSTY_1418)
 
-cov.data.files <- c("data/aoi.covdata.1996.csv","data/retro.covdata.1996.csv",
-                    "data/aoi.covdata.1997.csv","data/retro.covdata.1997.csv",
-                    "data/aoi.covdata.2020.csv","data/rec.covdata.2020.csv")
+names.cov.df <- names(cov.df)
+col96 <- names.cov.df[grepl("retro|96",names.cov.df)]
+col96 <- col96[!grepl("97",col96)]
+cov_retro_96 <- cov.df %>% dplyr::select("grid_id","SBS_prop","RLW_dist",all_of(col96))
+cov_retro_96$Year <- "1996-1997"
 
-ALLcov.df <-do.call(rbind,lapply(cov.data.files, read.csv))
-ALLcov.df$X <- ALLcov.df$grid_id <- NULL
-cov.df.nrows <- sapply( cov.data.files, function(f) nrow(read.csv(f)) )
+col97 <- names.cov.df[grepl("retro|97",names.cov.df)]
+col97 <- col97[!grepl("96",col97)]
+cov_retro_97 <- cov.df %>% dplyr::select("grid_id","SBS_prop","RLW_dist",all_of(col97))
+cov_retro_97$Year <- "1997-1998"
 
+col19 <- names.cov.df[grepl("recent|19",names.cov.df)]
+cov_recent_19 <- cov.df %>% dplyr::select("grid_id","SBS_prop","RLW_dist",all_of(col19))
+cov_recent_19$Year <- "2019-2020"
 
-ALLcov.df$Year <- c(rep(1996,cov.df.nrows[1]+cov.df.nrows[2]),
-                    rep(1997,cov.df.nrows[3]+cov.df.nrows[4]), 
-                    rep(2019,cov.df.nrows[5]+cov.df.nrows[6]))
-ALLcov.df$Area <- c(rep("Area",cov.df.nrows[1]), rep("Traps",cov.df.nrows[2]),
-                    rep("Area",cov.df.nrows[3]), rep("Traps",cov.df.nrows[4]),
-                    rep("Area",cov.df.nrows[5]), rep("Traps",cov.df.nrows[6]))
-ALLcov.df %>% group_by(Year) %>% count(Area)
-ALLcov.df$Area_Year <- paste(ALLcov.df$Area, ALLcov.df$Year)
+names(cov_retro_96);names(cov_retro_97);names(cov_recent_19)
+new.names.cov.df <- c("grid_id","SBS_prop","RLW_dist","RD_density","TREE20_prop","CANOPY_prop",
+                      "EDGE_density","HARVEST_prop","Area","TRP_DNSTY","Year")
+names(cov_retro_96) <- names(cov_retro_97) <- names(cov_recent_19) <- new.names.cov.df
 
-ALLcov.df_longer <- ALLcov.df %>% dplyr::select(-RLW_type,-Area,-Year) %>%
+ALLcov.df <- rbind(cov_retro_96, cov_retro_96 %>% filter(Area==1), 
+                   cov_retro_97, cov_retro_97 %>% filter(Area==1),
+                   cov_recent_19, cov_recent_19 %>% filter(Area==1))
+
+nrow(cov_retro_96 %>% filter(Area==1)); nrow(cov_retro_97%>% filter(Area==1)); nrow(cov_recent_19 %>% filter(Area==1))
+nrow(cov_retro_96); nrow(cov_retro_97); nrow(cov_recent_19)
+
+ALLcov.df$Area_Year <- c(rep("Area 1996",times=nrow(cov_retro_96)),
+                         rep("Traps 1996", times=nrow(cov_retro_96 %>% filter(Area==1))),
+                         rep("Area 1997",times=nrow(cov_retro_97)),
+                         rep("Traps 1997", times=nrow(cov_retro_97 %>% filter(Area==1))),
+                         rep("Area 2019",times=nrow(cov_recent_19)),
+                         rep("Traps 2019", times=nrow(cov_recent_19 %>% filter(Area==1))))
+
+ALLcov.df %>% group_by(Area_Year) %>% count(Area,Year)
+
+ALLcov.df_longer <- ALLcov.df %>% dplyr::select(-Area,-Year,-grid_id) %>%
   pivot_longer(!(Area_Year), names_to = "Covariate", values_to = "Values")
 
 names(ALLcov.df_longer)
 unique(ALLcov.df_longer$Covariate)
 ALLcov.df_longer$Covariate <- as.factor(recode(ALLcov.df_longer$Covariate, SBS_prop = "Proportion SBS", RLW_dist = "Distance to Water",
        RD_density = "Road Density", TREE20_prop = "Proportion Trees > 20 m", CANOPY_prop = "Proportion Canopy > 45%",
-       EDGE_density = "Forest Edge Density", HARVEST_prop = "Proportion Harvested"))
+       EDGE_density = "Forest Edge Density", HARVEST_prop = "Proportion Harvested", TRP_DNSTY = "Marten Trap Harvest Density"))
 levels(ALLcov.df_longer$Covariate)
 
-ALLcov.df_longer$Covariate <- fct_relevel(ALLcov.df_longer$Covariate, "Proportion Canopy > 45%", "Proportion Harvested","Proportion SBS", "Proportion Trees > 20 m",
-            "Distance to Water","Forest Edge Density", "Road Density")
+ALLcov.df_longer$Covariate <- fct_relevel(ALLcov.df_longer$Covariate, "Proportion SBS", "Proportion Trees > 20 m","Proportion Canopy > 45%", "Distance to Water","Proportion Harvested",
+            "Forest Edge Density", "Road Density", "Marten Trap Harvest Density")
 
 ALLcov.df_longer$Area_Year <- fct_relevel(ALLcov.df_longer$Area_Year,  "Traps 1996","Area 1996", "Traps 1997","Area 1997","Traps 2019","Area 2019")
 
 pal = pnw_palette(name="Cascades",n=6,type="discrete")
 
-cov.plot <- ggplot(ALLcov.df_longer, aes(x=Covariate, y=Values, fill=as.factor(Area_Year))) +
+cov.plot <- ggplot(ALLcov.df_longer %>% filter(Covariate!="Marten Trap Harvest Density"),
+                   aes(x=Covariate, y=Values, fill=as.factor(Area_Year))) +
   geom_boxplot(notch=FALSE) +
   facet_wrap(~Covariate, scale="free",nrow=2)+
   theme(axis.title.x=element_blank(),axis.title.y=element_blank(),axis.text.x=element_blank()) +
@@ -91,22 +126,169 @@ Cairo(file="out/marten_ALLcov_plot_969719.PNG",type="png",width=3400,height=2400
 cov.plot
 dev.off()
 
-
 # Run occupancy models for unmarked and marked live trap data
 # consider that male home ranges ~5.25 km2 and female home ranges ~3.16 km2 (Eric Lofroth's MSc thesis)
 # larger in fire disturbed landscapes in BC/WA during winter, ~13 km2 for males and ~8.5 km2 for females  (Volkmann & Hodges 2021)
 
-# source("03_analysis_prep.R") # if not working concurrently
-
 # 1999/00 detections high for first 3 weeks and then drop off - unlikely for models to converge
 # different sampling effort in 1999/00 - concentrating only on fisher and moving traps to target recapturing fisher
-# 1998/99 might also be worth ignoring as trapping effort became much more focused on fisher
+# ignoring 1998/99 as well, as trapping effort became much more focused on fisher
 #####################################################################################
-# borrowed code from https://jamesepaterson.github.io/jamespatersonblog/2020-09-01_occupancyintroduction.html
+# Following from Kery and Chandler for 'Dynamic occupancy models in unmarked'
+# https://cran.r-project.org/web/packages/unmarked/vignettes/colext.pdf
+# site = i; survey = j; season=t; yijt = 1 if detected
+# replicate surveys at a site during a single season are indpendent (or dependency must be modeled)
+# occurrence state does not change over replicate surveys at site i during season t
+# there are no false positive errors
 
-# Load detection history (100 sites with 10 visits each)
-detection_history <- retro.data.out[[2]]$y_21day
-# detection_history <- rec.data.out$rec_ydata
+M <- length(ALLcov.df_grid_to_use) # 150; number of sites
+J <- max(length(retro.data.out[[1]]$days21.to.use),  # number of sampling occassions
+         length(retro.data.out[[2]]$days21.to.use),
+         ncol(rec.data.out$rec_effort))
+T <- 3 # number of years / sesasons
+
+
+# data(crossbill)
+# head(crossbill); nrow(crossbill)
+# summary(crossbill)
+head(ALLcov.df)
+
+ALLcov.df_grid_to_use <- ALLcov.df %>% filter(grepl("Traps", Area_Year)) %>% select(grid_id)
+ALLcov.df_grid_to_use <- unique(ALLcov.df_grid_to_use$grid_id)
+length(ALLcov.df_grid_to_use)
+
+# create site covariates
+colext.df <- ALLcov.df %>% filter(grid_id %in% ALLcov.df_grid_to_use) %>% filter(grepl("Area",Area_Year))
+colext.df$RLW_dist_sq <- colext.df$RLW_dist*colext.df$RLW_dist
+colext.df$RD_density_sq <- colext.df$RD_density*colext.df$RD_density
+colext.df$EDGE_density_sq <- colext.df$EDGE_density*colext.df$EDGE_density
+colext.site <- colext.df %>% select(-grid_id, -Year, -Area_Year, -Area)
+head(colext.site)
+
+# scale the site covariates
+colext.site <- as.data.frame(scale(colext.site))
+colext.site[is.na(colext.site)] <- 0 # do this to make the NAs for trap density the same as the mean value (could also just delete but then deletes from model)
+summary(colext.site)
+colext.site <- as.matrix(colext.site)
+
+# detection data
+y_all <- array(NA,dim=c(length(ALLcov.df_grid_to_use),0))
+rownames(y_all) <- as.character(ALLcov.df_grid_to_use)
+
+y96.df <- retro.data.out[[1]]$y_21day
+y96 <- merge(y_all, y96.df, by="row.names", all.x=TRUE)
+y96 <- arrange(y96, Row.names)
+rownames(y96) <- y96$Row.names
+y96$Row.names <- NULL
+y96 <- as.matrix(y96)
+y96[y96 > 0] <- 1 # change to 0 and 1 only
+
+
+y97.df <- retro.data.out[[2]]$y_21day
+y97 <- merge(y_all, y97.df, by="row.names", all.x=TRUE)
+y97 <- arrange(y97, Row.names)
+rownames(y97) <- y97$Row.names
+y97$Row.names <- NULL
+y97 <- as.matrix(y97)
+y97[y97 > 0] <- 1 # change to 0 and 1 only
+
+
+y19.df <- rec.data.out$rec_ydata
+y19 <- merge(y_all, y19.df, by="row.names", all.x=TRUE)
+y19 <- arrange(y19, Row.names)
+rownames(y19) <- y19$Row.names
+y19$Row.names <- NULL
+y19 <- as.matrix(y19)
+y19[y19 > 0] <- 1 # change to 0 and 1 only
+
+y <- array(NA, dim=c(M,J,T))
+y[,,1] <- cbind(y96, array(NA, dim=c(nrow(y96),J-ncol(y96))))
+y[,,2] <- cbind(y97, array(NA, dim=c(nrow(y97),J-ncol(y97))))
+y[,,3] <- cbind(y19, array(NA, dim=c(nrow(y19),J-ncol(y19))))
+
+sum(y, na.rm=T); sum(sum(y96, na.rm=T),sum(y97, na.rm = T),sum(y19, na.rm = T))
+
+yy <- matrix(y, M, J*T)
+
+# yearly covariates (think it's just one row for each site...)
+year <- c("1996-1997","1997-1998","2019-2020")
+year <- matrix(year, nrow(yy),3,byrow=TRUE)
+
+simUMF <- unmarkedMultFrame(
+  y = yy,
+  yearlySiteCovs = list(year = year),
+  numPrimary=T)
+summary(simUMF)
+
+# Model with all constant parameters
+m0 <- colext(psiformula= ~1, gammaformula = ~ 1, epsilonformula = ~ 1,
+                 pformula = ~ 1, data = simUMF, method="BFGS")
+summary(m0)
+
+names(m0)
+# [1] "psi" "col" "ext" "det"
+# occupancy, colonization, extinction, detection
+
+# backTransform(m0, type="psi")
+# backTransform(m0, type="col")
+# backTransform(m0, type="ext")
+# backTransform(m0, type="det")
+
+confint(backTransform(m0, type="psi"))
+
+
+m1 <- colext(psiformula = ~1, # First-year occupancy
+               gammaformula = ~ year-1, # Colonization
+               epsilonformula = ~ year-1, # Extinction
+               pformula = ~ year-1, # Detection
+               data = simUMF)
+m1
+
+
+nd <- data.frame(year=c("1996-1997","1997-1998"))
+E.ext <- predict(m1, type='ext', newdata=nd)
+E.col <- predict(m1, type='col', newdata=nd)
+nd <- data.frame(year=c("1996-1997","1997-1998","2019-2020"))
+E.det <- predict(m1, type='det', newdata=nd)
+
+op <- par(mfrow=c(3,1), mai=c(0.6, 0.6, 0.1, 0.1))
+with(E.ext, { # Plot for extinction probability
+  plot(1:2, Predicted, pch=1, xaxt='n', xlab='Year',
+       ylab=expression(paste('Extinction probability ( ', epsilon, ' )')),
+       ylim=c(0,1), col=4)
+  axis(1, at=1:2, labels=nd$year[1:2])
+  arrows(1:2, lower, 1:2, upper, code=3, angle=90, length=0.03, col=4)
+  points((1:2)-0.1, 1-phi, col=1, lwd = 1, pch=16)
+  legend(7, 1, c('Parameter', 'Estimate'), col=c(1,4), pch=c(16, 1),
+         cex=0.8)
+})
+
+with(E.col, { # Plot for colonization probability
+  plot(1:2, Predicted, pch=1, xaxt='n', xlab='Year',
+       ylab=expression(paste('Colonization probability ( ', gamma, ' )')),
+       ylim=c(0,1), col=4)
+  axis(1, at=1:2, labels=nd$year[1:2])
+  arrows(1:2, lower, 1:2, upper, code=3, angle=90, length=0.03, col=4)
+  points((1:2)-0.1, gamma, col=1, lwd = 1, pch=16)
+  legend(7, 1, c('Parameter', 'Estimate'), col=c(1,4), pch=c(16, 1),
+         cex=0.8)
+})
+with(E.det, { # Plot for detection probability: note 3 years
+  plot(1:3, Predicted, pch=1, xaxt='n', xlab='Year',
+       ylab=expression(paste('Detection probability ( ', p, ' )')),
+       ylim=c(0,1), col=4)
+  axis(1, at=1:3, labels=nd$year)
+  arrows(1:3, lower, 1:3, upper, code=3, angle=90, length=0.03, col=4)
+  points((1:3)-0.1, p, col=1, lwd = 1, pch=16)
+  legend(7.5, 1, c('Parameter','Estimate'), col=c(1,4), pch=c(16, 1),
+         cex=0.8)
+})
+par(op)
+
+
+# detection_history <- retro.data.out[[1]]$y_21day
+# detection_history <- retro.data.out[[2]]$y_21day
+detection_history <- rec.data.out$rec_ydata
 detection_history[detection_history > 0] <- 1 # change to 0 and 1 only
 
 # Examine data
@@ -142,15 +324,15 @@ boot::inv.logit(coef(occu.m1)[1]) # Real estimate of occupancy
 boot::inv.logit(coef(occu.m1)[2]) # Real estimate of detection
 
 # Load covariate data
-grid_centroid_utm <- st_coordinates(st_centroid(retro.data.out[[2]]$grid_output$fishnet_grid_sf))
-cov.df$utm_x <- grid_centroid_utm[,1]
-cov.df$utm_y <- grid_centroid_utm[,2]
+head(ALLcov.df)
+grid_centroid_utm <- as.data.frame(st_coordinates(st_centroid(aoi_grid)))
 
-cov.df$RLW_type <- cov.df$grid_id <- NULL
+ALLcov.df$utm_x <- grid_centroid_utm$X[match(ALLcov.df$grid_id, rownames(grid_centroid_utm))]
+ALLcov.df$utm_y <- grid_centroid_utm$Y[match(ALLcov.df$grid_id, rownames(grid_centroid_utm))]
 
-# cov.df$utm_x <- rec.data.out$grid_centroid_utm[,1]
-# cov.df$utm_y <- rec.data.out$grid_centroid_utm[,2]
-
+# cov.df <- ALLcov.df %>% filter(Area_Year=="Traps 1996") %>% select(-grid_id, -Area, -Year, -Area_Year)
+# cov.df <- ALLcov.df %>% filter(Area_Year=="Traps 1997") %>% select(-grid_id, -Area, -Year, -Area_Year)
+cov.df <- ALLcov.df %>% filter(Area_Year=="Traps 2019") %>% select(-grid_id, -Area, -Year, -Area_Year)
 summary(cov.df)
 
 cov.df$RLW_dist_sq <- cov.df$RLW_dist*cov.df$RLW_dist
@@ -161,23 +343,24 @@ summary(cov.df)
 
 cov.df_scaled <- as.data.frame(scale(cov.df))
 summary(cov.df_scaled)
-
+cov.df_scaled[is.na(cov.df_scaled)] <- 0
 
 # scale(x,center=min(x),scale=diff(range(x)))
 # https://stackoverflow.com/questions/5468280/scale-a-series-between-two-points
 
-effort <- retro.data.out[[2]]$effort.21days
-# effort <- rec.data.out$rec_effort
+# effort <- retro.data.out[[1]]$effort.21days
+# effort <- retro.data.out[[2]]$effort.21days
+effort <- rec.data.out$rec_effort
 
 # Build a new unmarkedFramOccu
-sample.unmarkedFrame_cov <- unmarkedFrameOccu( # y is a matrix with observed detection history 
-  # (0's and 1's, one row per site, one column per survey)
-  y = as.matrix(detection_history),
-  # obsCovs = observation covariates in a list, 
-  # each variable has site rows x survey columns
-  obsCovs = list(effort = effort),
-  # siteCovs = dataframe with site rows x column variables
-  siteCovs = cov.df)
+# sample.unmarkedFrame_cov <- unmarkedFrameOccu( # y is a matrix with observed detection history 
+#   # (0's and 1's, one row per site, one column per survey)
+#   y = as.matrix(detection_history),
+#   # obsCovs = observation covariates in a list, 
+#   # each variable has site rows x survey columns
+#   obsCovs = list(effort = effort),
+#   # siteCovs = dataframe with site rows x column variables
+#   siteCovs = cov.df)
 
 sample.unmarkedFrame_cov_scaled <- unmarkedFrameOccu( # y is a matrix with observed detection history 
   # (0's and 1's, one row per site, one column per survey)
@@ -196,7 +379,7 @@ occu.m2 <- occu(formula = ~effort # detection formula first
 
 
 occu.full <- occu(formula = ~effort # detection formula first
-                ~ utm_x + utm_y + SBS_prop + RLW_dist + RLW_dist_sq + RD_density + RD_density_sq + TREE20_prop + 
+                ~ TRP_DNSTY + utm_x + utm_y + SBS_prop + RLW_dist + RLW_dist_sq + RD_density + RD_density_sq + TREE20_prop + 
                   CANOPY_prop + EDGE_density + EDGE_density_sq + HARVEST_prop, # occupancy formula second,
                 data = sample.unmarkedFrame_cov_scaled)
 
